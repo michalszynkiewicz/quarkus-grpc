c290e617f38bbf656aab602d145b3f97ce10216c
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-05 10:58:30 +0200

    Provide Native support

--- deployment/src/main/java/io/quarkus/grpc/deployment/GrpcDotNames.java
+++ deployment/src/main/java/io/quarkus/grpc/deployment/GrpcDotNames.java
@@ -1,6 +1,9 @@
 package io.quarkus.grpc.deployment;
 
+import com.google.protobuf.GeneratedMessageV3;
 import io.grpc.Channel;
+import io.grpc.LoadBalancerProvider;
+import io.grpc.NameResolverProvider;
 import io.quarkus.gizmo.MethodDescriptor;
 import io.quarkus.grpc.runtime.annotations.GrpcService;
 import io.quarkus.grpc.runtime.supports.Channels;
@@ -10,6 +13,10 @@ public class GrpcDotNames {
 
     static final DotName CHANNEL = DotName.createSimple(Channel.class.getName());
     static final DotName GRPC_SERVICE = DotName.createSimple(GrpcService.class.getName());
+    static final DotName MESSAGE_BUILDER = DotName.createSimple(GeneratedMessageV3.Builder.class.getName());
+    static final DotName GENERATED_MESSAGE_V3 = DotName.createSimple(GeneratedMessageV3.class.getName());
+    static final DotName NAME_RESOLVER_PROVIDER = DotName.createSimple(NameResolverProvider.class.getName());
+    static final DotName LOAD_BALANCER_PROVIDER = DotName.createSimple(LoadBalancerProvider.class.getName());
 
 
     static final MethodDescriptor CREATE_CHANNEL_METHOD = MethodDescriptor.ofMethod(Channels.class, "createChannel", Channel.class, String.class);
--- deployment/src/main/java/io/quarkus/grpc/deployment/GrpcProcessor.java
+++ deployment/src/main/java/io/quarkus/grpc/deployment/GrpcProcessor.java
@@ -1,6 +1,9 @@
 package io.quarkus.grpc.deployment;
 
 import io.grpc.Channel;
+import io.grpc.internal.DnsNameResolverProvider;
+import io.grpc.internal.PickFirstLoadBalancerProvider;
+import io.grpc.netty.NettyChannelProvider;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.deployment.BeanRegistrationPhaseBuildItem;
 import io.quarkus.arc.processor.BeanConfigurator;
@@ -10,9 +13,12 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.ServiceStartBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.gizmo.MethodCreator;
 import io.quarkus.gizmo.MethodDescriptor;
 import io.quarkus.gizmo.ResultHandle;
@@ -21,14 +27,12 @@ import io.quarkus.grpc.runtime.config.GrpcServerConfiguration;
 import io.quarkus.grpc.runtime.GrpcServerRecorder;
 import io.quarkus.grpc.runtime.annotations.GrpcService;
 import io.quarkus.vertx.deployment.VertxBuildItem;
-import org.jboss.jandex.AnnotationInstance;
-import org.jboss.jandex.ClassType;
-import org.jboss.jandex.DotName;
-import org.jboss.jandex.Type;
+import org.jboss.jandex.*;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.inject.spi.DeploymentException;
 import javax.inject.Singleton;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -186,6 +190,40 @@ public class GrpcProcessor {
             VertxBuildItem vertx) {
         recorder.initializeGrpcServer(config, shutdown);
         return new ServiceStartBuildItem("grpc-server");
+    }
+
+    @BuildStep
+    public void configureNativeExecutable(CombinedIndexBuildItem combinedIndex,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
+            BuildProducer<ExtensionSslNativeSupportBuildItem> extensionSslNativeSupport) {
+
+        // we force the usage of the reflection invoker.
+        Collection<ClassInfo> messages = combinedIndex.getIndex().getAllKnownSubclasses(GrpcDotNames.GENERATED_MESSAGE_V3);
+        for (ClassInfo message : messages) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, true, message.name().toString()));
+        }
+        Collection<ClassInfo> builders = combinedIndex.getIndex().getAllKnownSubclasses(GrpcDotNames.MESSAGE_BUILDER);
+        for (ClassInfo builder : builders) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, true, builder.name().toString()));
+        }
+
+        Collection<ClassInfo> lbs = combinedIndex.getIndex().getAllKnownSubclasses(GrpcDotNames.LOAD_BALANCER_PROVIDER);
+        for (ClassInfo lb : lbs) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, false, lb.name().toString()));
+        }
+
+        Collection<ClassInfo> nrs = combinedIndex.getIndex().getAllKnownSubclasses(GrpcDotNames.NAME_RESOLVER_PROVIDER);
+        for (ClassInfo nr : nrs) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, false, nr.name().toString()));
+        }
+
+        // Built-In providers:
+        reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, false, DnsNameResolverProvider.class));
+        reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, false, PickFirstLoadBalancerProvider.class));
+        reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, false, "io.grpc.util.SecretRoundRobinLoadBalancerProvider$Provider"));
+        reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, false, NettyChannelProvider.class));
+
+        extensionSslNativeSupport.produce(new ExtensionSslNativeSupportBuildItem(GRPC));
     }
 
 }
