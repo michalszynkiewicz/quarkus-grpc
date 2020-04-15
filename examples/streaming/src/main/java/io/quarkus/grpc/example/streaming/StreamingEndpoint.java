843d1d98495261db1ad8c0ae2cf3f8d2d7dccf8b
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-15 19:40:15 +0200

    Write the documentation

--- examples/streaming/src/main/java/io/quarkus/grpc/example/streaming/StreamingEndpoint.java
+++ examples/streaming/src/main/java/io/quarkus/grpc/example/streaming/StreamingEndpoint.java
@@ -5,6 +5,7 @@ import io.grpc.examples.streaming.Item;
 import io.grpc.examples.streaming.MutinyStreamingGrpc;
 import io.quarkus.grpc.runtime.annotations.GrpcService;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -20,18 +21,26 @@ public class StreamingEndpoint {
     @Inject @GrpcService("streaming") MutinyStreamingGrpc.MutinyStreamingStub client;
 
     @GET
-    public Multi<String> stream() {
+    public Multi<String> invokeSource() {
         return client.source(Empty.newBuilder().build())
                 .onItem().apply(Item::getValue);
     }
 
     @GET
+    @Path("sink/{max}")
+    public Uni<Void> invokeSink(@PathParam("max") int max) {
+        Multi<Item> inputs = Multi.createFrom().range(0, max)
+                .map(i -> Integer.toString(i))
+                .map(i -> Item.newBuilder().setValue(i).build());
+        return client.sink(inputs).onItem().ignore().andContinueWithNull();
+    }
+
+    @GET
     @Path("/{max}")
-    public Multi<String> pipe(@PathParam("max") int max) {
+    public Multi<String> invokePipe(@PathParam("max") int max) {
         Multi<Item> inputs = Multi.createFrom().range(0, max)
                 .map(i -> Integer.toString(i))
                 .map(i -> Item.newBuilder().setValue(i).build());
         return client.pipe(inputs).onItem().apply(Item::getValue);
     }
-
 }
